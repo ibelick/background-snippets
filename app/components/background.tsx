56974089d49ecc39cadd9ce9b50090a0a264b6d5
ibelick <julien.thibeaut@gmail.com>
2023-07-11 23:15:06 +0200

    feat: add prettier

--- app/components/background.tsx
+++ app/components/background.tsx
@@ -1,12 +1,12 @@
 const Background1 = () => {
   return (
-    <div className="bg-white absolute inset-0 -z-10 h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]" />
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]" />
   );
 };
 
 const Background18 = () => {
   return (
-    <div className="relative bg-white h-full w-full">
+    <div className="relative h-full w-full bg-white">
       <div className="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]" />
     </div>
   );
@@ -14,29 +14,29 @@ const Background18 = () => {
 
 const Background22 = () => {
   return (
-    <div className="relative bg-white h-full w-full">
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)] " />
+    <div className="relative h-full w-full bg-white">
+      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
     </div>
   );
 };
 
 const Background2 = () => {
   return (
-    <div className="bg-white absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]" />
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]" />
     </div>
   );
 };
 
 const Background3 = () => {
   return (
-    <div className="bg-white absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]" />
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]" />
   );
 };
 
 const Background4 = () => {
   return (
-    <div className="bg-white absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
       <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]" />
     </div>
   );
@@ -50,13 +50,13 @@ const Background5 = () => {
 
 const Background6 = () => {
   return (
-    <div className="bg-white absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]" />
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]" />
   );
 };
 
 const Background7 = () => {
   return (
-    <div className="bg-white absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]" />
+    <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]" />
   );
 };
 
@@ -68,13 +68,13 @@ const Background8 = () => {
 
 const Background9 = () => {
   return (
-    <div className="bg-white absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]" />
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]" />
   );
 };
 
 const Background10 = () => {
   return (
-    <div className="bg-white absolute top-0 -z-10 h-full w-full">
+    <div className="absolute top-0 -z-10 h-full w-full bg-white">
       <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]" />
     </div>
   );
@@ -82,13 +82,13 @@ const Background10 = () => {
 
 const Background11 = () => {
   return (
-    <div className="bg-white absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
   );
 };
 
 const Background12 = () => {
   return (
-    <div className="bg-white absolute inset-0 -z-10 h-full w-full [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]" />
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]" />
   );
 };
 
@@ -100,171 +100,171 @@ const Background13 = () => {
 
 const Background14 = () => {
   return (
-    <div className="relative bg-neutral-900 h-full w-full">
-      <div className="absolute inset-0 bg-fuchsia-400 opacity-20 blur-[100px] bg-[size:20px_20px]"></div>
+    <div className="relative h-full w-full bg-neutral-900">
+      <div className="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
     </div>
   );
 };
 
 const Background15 = () => {
   return (
-    <div className="bg-white absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]" />
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]" />
     </div>
   );
 };
 
 const Background16 = () => {
   return (
-    <div className="relative bg-slate-950 h-full w-full">
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]" />
+    <div className="relative h-full w-full bg-slate-950">
+      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]" />
     </div>
   );
 };
 
 const Background17 = () => {
   return (
-    <div className="relative bg-slate-950 h-full w-full">
-      <div className="absolute top-[-10%] left-[-20%] rounded-full right-0 w-[500px] h-[500px] bottom-0 bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]" />
-      <div className="absolute top-[-10%] right-[-20%] rounded-full w-[500px] h-[500px] bottom-0 bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]" />
+    <div className="relative h-full w-full bg-slate-950">
+      <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]" />
+      <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]" />
     </div>
   );
 };
 
 const Background19 = () => {
   return (
-    <div className="relative bg-slate-950 h-full w-full">
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
+    <div className="relative h-full w-full bg-slate-950">
+      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
     </div>
   );
 };
 
 const Background20 = () => {
   return (
-    <div className="relative bg-black h-full w-full">
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]" />
-      <div className="absolute top-[-10%] left-[-20%] rounded-full right-0 w-[1000px] h-[1000px] bg-[radial-gradient(circle_400px_at_30%_300px,#fbfbfb36,#000)]" />
+    <div className="relative h-full w-full bg-black">
+      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]" />
+      <div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]" />
     </div>
   );
 };
 
 const Background21 = () => {
   return (
-    <div className="relative bg-slate-950 h-full w-full">
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
+    <div className="relative h-full w-full bg-slate-950">
+      <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
     </div>
   );
 };
 
 export const BACKGROUND_OPTIONS = [
   {
-    name: "Background 1",
+    name: 'Background 1',
     component: <Background1 />,
-    theme: "light",
+    theme: 'light',
   },
   {
-    name: "Background 18",
+    name: 'Background 18',
     component: <Background18 />,
-    theme: "light",
+    theme: 'light',
   },
   {
-    name: "Background 2",
+    name: 'Background 2',
     component: <Background2 />,
-    theme: "light",
+    theme: 'light',
   },
   {
-    name: "Background 3",
+    name: 'Background 3',
     component: <Background3 />,
-    theme: "light",
+    theme: 'light',
   },
   {
-    name: "Background 4",
+    name: 'Background 4',
     component: <Background4 />,
-    theme: "light",
+    theme: 'light',
   },
   {
-    name: "Background 6",
+    name: 'Background 6',
     component: <Background6 />,
-    theme: "light",
+    theme: 'light',
   },
   {
-    name: "Background 2",
+    name: 'Background 2',
     component: <Background15 />,
-    theme: "light",
+    theme: 'light',
   },
   {
-    name: "Background 7",
+    name: 'Background 7',
     component: <Background7 />,
-    theme: "light",
+    theme: 'light',
   },
   {
-    name: "Background 9",
+    name: 'Background 9',
     component: <Background9 />,
-    theme: "light",
+    theme: 'light',
   },
   {
-    name: "Background 10",
+    name: 'Background 10',
     component: <Background10 />,
-    theme: "light",
+    theme: 'light',
   },
   {
-    name: "Background 22",
+    name: 'Background 22',
     component: <Background22 />,
-    theme: "light",
+    theme: 'light',
   },
   {
-    name: "Background 11",
+    name: 'Background 11',
     component: <Background11 />,
-    theme: "light",
+    theme: 'light',
   },
   {
-    name: "Background 12",
+    name: 'Background 12',
     component: <Background12 />,
-    theme: "light",
+    theme: 'light',
   },
   {
-    name: "Background 13",
+    name: 'Background 13',
     component: <Background13 />,
-    theme: "dark",
+    theme: 'dark',
   },
   {
-    name: "Background 5",
+    name: 'Background 5',
     component: <Background5 />,
-    theme: "dark",
+    theme: 'dark',
   },
   {
-    name: "Background 8",
+    name: 'Background 8',
     component: <Background8 />,
-    theme: "dark",
+    theme: 'dark',
   },
   {
-    name: "Background 14",
+    name: 'Background 14',
     component: <Background14 />,
-    theme: "dark",
+    theme: 'dark',
   },
   {
-    name: "Background 16",
+    name: 'Background 16',
     component: <Background16 />,
-    theme: "dark",
+    theme: 'dark',
   },
   {
-    name: "Background 17",
+    name: 'Background 17',
     component: <Background17 />,
-    theme: "dark",
+    theme: 'dark',
   },
   {
-    name: "Background 19",
+    name: 'Background 19',
     component: <Background19 />,
-    theme: "dark",
+    theme: 'dark',
   },
   {
-    name: "Background 20",
+    name: 'Background 20',
     component: <Background20 />,
-    theme: "dark",
+    theme: 'dark',
   },
   {
-    name: "Background 21",
+    name: 'Background 21',
     component: <Background21 />,
-    theme: "dark",
+    theme: 'dark',
   },
 ] as const;
--- app/components/playground.tsx
+++ app/components/playground.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOMServer from "react-dom/server";
-import { toast } from "sonner";
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { toast } from 'sonner';
 
 type PlaygroundProps = {
   children: React.ReactElement;
   setPreview: (preview: React.ReactNode) => void;
-  theme: "light" | "dark";
-  setTheme: (theme: "light" | "dark") => void;
+  theme: 'light' | 'dark';
+  setTheme: (theme: 'light' | 'dark') => void;
 };
 
 const Playground: React.FC<PlaygroundProps> = ({
@@ -19,15 +19,16 @@ const Playground: React.FC<PlaygroundProps> = ({
     const code = ReactDOMServer.renderToString(children);
 
     navigator.clipboard.writeText(code);
-    toast.success("Copied to clipboard");
+    toast.success('Copied to clipboard');
   };
+
   return (
     <>
       <div className="relative min-h-[300px] w-full overflow-hidden rounded-lg ring-1 ring-slate-900/10">
-        <div className="absolute cursor-pointer top-4 left-4 z-10">
+        <div className="absolute left-4 top-4 z-10 cursor-pointer">
           <div className="flex flex-row gap-4">
             <div
-              className=" bg-slate-900 text-white text-xs font-medium px-2 py-1 rounded-md"
+              className=" rounded-md bg-slate-900 px-2 py-1 text-xs font-medium text-white"
               onClick={() => {
                 setPreview(children);
                 setTheme(theme);
@@ -36,7 +37,7 @@ const Playground: React.FC<PlaygroundProps> = ({
               preview
             </div>
             <div
-              className="bg-slate-900 text-white text-xs font-medium px-2 py-1 rounded-md"
+              className="rounded-md bg-slate-900 px-2 py-1 text-xs font-medium text-white"
               onClick={copyCode}
             >
               copy code
