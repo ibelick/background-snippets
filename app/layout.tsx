57d2b2707e5a5932a1e3311d00c4c638044e0861
ibelick <julien.thibeaut@gmail.com>
2023-07-14 15:32:32 +0200

    feat: add umami script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -14,8 +15,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const isDev = process.env.NODE_ENV === 'development';
+
   return (
     <html lang="en">
+      {!isDev ? (
+        <Script
+          async
+          src="https://analytics.umami.is/script.js"
+          data-website-id="9ad0e597-9fa4-4690-8ca2-18a964ab087f"
+        />
+      ) : null}
       <body className={inter.className}>{children}</body>
     </html>
   );
