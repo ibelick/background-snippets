bec30edff48754796bbc3441b2419ad4fb72a991
HlexNC <hlexhelftd@gmail.com>
2024-09-10 14:09:21 +0200

    feat: enhance SEO with metadata and Open Graph integration
    - Updated title and description to improve clarity and search engine optimization.
    - Added relevant keywords for better indexing.
    - Implemented Open Graph (OG) tags for improved social media sharing, including title, description, and image metadata.
    - Added `banner.jpg` for OG image with recommended dimensions (1200x630px) for better social media previews.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,23 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'bg.ibelick - ready-to-use background snippets for web developers',
-  description:
-    'Ready-to-use, simply copy and paste into your next project. All snippets crafted with Tailwind CSS and Vanilla CSS for easy integration.',
+  title: 'BG.IBELICK - Modern, Ready-to-Use Background Snippets for Developers',
+  description: 'A collection of modern background snippets for web developers. Ready-to-use snippets crafted with Tailwind CSS and Vanilla CSS for seamless integration.',
+  keywords: 'background snippets, Tailwind CSS, CSS snippets, web development, front-end snippets, free CSS resources',
+  openGraph: {
+    title: 'BG.IBELICK - Ready-to-Use Background Snippets',
+    description: 'Modern background snippets for web developers crafted with Tailwind CSS and Vanilla CSS.',
+    url: 'https://bg.ibelick.com',
+    type: 'website',
+    images: [
+      {
+        url: '/banner.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'BG.IBELICK Cover Image',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -19,6 +33,19 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
+        <meta property="og:image:width" content={metadata.openGraph.images[0].width.toString()} />
+        <meta property="og:image:height" content={metadata.openGraph.images[0].height.toString()} />
+      </head>
       {!isDev ? (
         <Script
           async
