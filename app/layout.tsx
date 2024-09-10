d93c2e599fbcbb981207ecbe0c00ab960b977f49
HlexNC <hlexhelftd@gmail.com>
2024-09-10 14:34:13 +0200

    banner fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,12 +39,12 @@ export default function RootLayout({
         <meta name="keywords" content={metadata.keywords} />
         <meta property="og:title" content={metadata.openGraph.title} />
         <meta property="og:description" content={metadata.openGraph.description} />
-        <meta property="og:url" content={metadata.openGraph.url} />
+        {/* <meta property="og:url" content={metadata.openGraph.url} /> */}
         <meta property="og:type" content={metadata.openGraph.type} />
         <meta property="og:image" content={metadata.openGraph.images[0].url} />
-        <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
-        <meta property="og:image:width" content={metadata.openGraph.images[0].width.toString()} />
-        <meta property="og:image:height" content={metadata.openGraph.images[0].height.toString()} />
+        {/* <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} /> */}
+        {/* <meta property="og:image:width" content={metadata.openGraph.images[0].width.toString()} /> */}
+        {/* <meta property="og:image:height" content={metadata.openGraph.images[0].height.toString()} /> */}
       </head>
       {!isDev ? (
         <Script
